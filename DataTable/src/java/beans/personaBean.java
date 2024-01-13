c71c4f86d64e1f2fcd30a87fce4a05818aea304b
Ignacio Rueda <105673585+Ignacio-Rueda@users.noreply.github.com>
2024-01-13 07:59:34 +0100

    Inicio comentarios

---
+++ DataTable/src/java/beans/personaBean.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package beans;
+
+/**
+ *
+ * @author Ignacio Rueda
+ */
+public class personaBean {
+
+    /**
+     * Creates a new instance of personaBean
+     */
+    public personaBean() {
+    }
+    
+}
